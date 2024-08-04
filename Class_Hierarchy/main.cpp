f5e83b6eaebaededce1513b9b54b468a5e3272fa
Alexander Nesterov <alex18nest@icloud.com>
2024-08-04 15:46:51 -0300

    Initial Commit

---
+++ Class_Hierarchy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Class_Hierarchy
+//
+//  Created by Александр Нестеров on 8/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
